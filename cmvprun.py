2804ae9fc226782e8a1c3ef55b3c966317f60bf6
jman2424 <j4m4l@outlook.com>
2025-05-08 22:45:12 +0100

    Update cmvprun.py

--- cmvprun.py
+++ cmvprun.py
@@ -147,7 +147,7 @@ def home():
 
 # ========== RUN SERVER LOCALLY ========== 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(
         host="0.0.0.0",
         port=port,
